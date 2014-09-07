3eaa92dec04f2438b6da51b729034c474ce586d5
Hamuko <hamuko@burakku.com>
2014-09-07 11:00:20 +0300

    Add --start= arg to begin scraping at certain torrent ID

--- Scraper.py
+++ Scraper.py
@@ -1,13 +1,24 @@
 from Database import Database
 from Nyaa import Nyaa, NyaaEntry
+import getopt
 import os
+import sys
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
-
 nt = Nyaa()
 db = Database(script_dir)
 
-for i in range(db.last_entry + 1, nt.last_entry + 1):
+arguments = sys.argv[1:]
+optlist, args = getopt.getopt(arguments, '', ['start='])
+if len(optlist) > 0:
+	for opt, arg in optlist:
+		if opt == '--start':
+			start_entry = int(arg)
+
+if 'start_entry' not in globals():
+	start_entry = db.last_entry + 1
+
+for i in range(start_entry + 1, nt.last_entry + 1):
 	entry = NyaaEntry('http://www.nyaa.se/?page=view&tid={}'.format(i))
 	if entry.exists == True:
 		if entry.category in db.categories and entry.sub_category in db.sub_categories:
