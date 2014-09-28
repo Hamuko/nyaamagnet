a980259ee7825fb96755e5f30cc207f3e398f15f
Hamuko <hamuko@burakku.com>
2014-09-28 16:21:03 +0300

    Make the code to conform to PEP 8 style guide.

--- Nyaa.py
+++ Nyaa.py
@@ -6,99 +6,112 @@ import time
 
 delay = 0
 
+
 def _retry_on_fail(req, *args, **kwargs):
-	global delay
-	try:
-		r = req(*args, **kwargs)
-		if r.status_code not in range(100, 399):
-			delay = delay * 2 + 5
-			print('Connection error, retrying in {} seconds... (HTTP {})'.format(delay, r.status_code), file=sys.stderr)
-			if delay > 1800:
-				print('Too many retry attempts.')
-				exit(code=1)
-			time.sleep(delay)
-			return _retry_on_fail(req, *args, **kwargs)
-		else:
-			delay = 0
-			return r
-	except requests.exceptions.RequestException as e:
-		delay = delay * 2 + 5
-		print('Connection error, retrying in {} seconds... ({})'.format(delay, e.args[0].args[0]), file=sys.stderr)
-		if delay > 1800:
-			print('Too many retry attempts.')
-			exit(code=1)
-		time.sleep(delay)
-		return _retry_on_fail(req, *args, **kwargs)
-	except requests.packages.urllib3.exceptions.ProtocolError as e:
-		delay = delay * 2 + 5
-		print('Connection error, retrying in {} seconds... ({})'.format(delay, e.args[1]), file=sys.stderr)
-		if delay > 1800:
-			print('Too many retry attempts.')
-			exit(code=1)
-		time.sleep(delay)
-		return _retry_on_fail(req, *args, **kwargs)
+    global delay
+    try:
+        r = req(*args, **kwargs)
+        if r.status_code not in range(100, 399):
+            delay = delay * 2 + 5
+            print('Connection error, retrying in {} seconds... '
+                  '(HTTP {})'.format(delay, r.status_code), file=sys.stderr)
+            if delay > 1800:
+                print('Too many retry attempts.')
+                exit(code=1)
+            time.sleep(delay)
+            return _retry_on_fail(req, *args, **kwargs)
+        else:
+            delay = 0
+            return r
+    except requests.exceptions.RequestException as e:
+        delay = delay * 2 + 5
+        print('Connection error, retrying in {} seconds... '
+              '({})'.format(delay, e.args[0].args[0]), file=sys.stderr)
+        if delay > 1800:
+            print('Too many retry attempts.')
+            exit(code=1)
+        time.sleep(delay)
+        return _retry_on_fail(req, *args, **kwargs)
+    except requests.packages.urllib3.exceptions.ProtocolError as e:
+        delay = delay * 2 + 5
+        print('Connection error, retrying in {} seconds... '
+              '({})'.format(delay, e.args[1]), file=sys.stderr)
+        if delay > 1800:
+            print('Too many retry attempts.')
+            exit(code=1)
+        time.sleep(delay)
+        return _retry_on_fail(req, *args, **kwargs)
+
 
 class Nyaa(object):
-	def __init__(self, url):
-		self.url = url
-		self.info_url = url + '?page=view&tid='
-		self.dl_url = url + '?page=download&tid='
+    def __init__(self, url):
+        self.url = url
+        self.info_url = url + '?page=view&tid='
+        self.dl_url = url + '?page=download&tid='
 
-	@property
-	def last_entry(self):
-		r = _retry_on_fail(requests.get, self.url)
-		soup = BeautifulSoup(r.text)
-		link = soup.find('tr', class_='tlistrow').find('td', class_='tlistname').a['href']
-		return int(re.search('tid=([0-9]*)', link).group(1))
+    @property
+    def last_entry(self):
+        r = _retry_on_fail(requests.get, self.url)
+        soup = BeautifulSoup(r.text)
+        link = soup.find('tr', class_='tlistrow') \
+                   .find('td', class_='tlistname').a['href']
+        return int(re.search('tid=([0-9]*)', link).group(1))
+
 
 class NyaaEntry(object):
-	def __init__(self, nyaa, nyaa_id):
-		self.info_url = '{}{}'.format(nyaa.info_url, nyaa_id)
-		self.download_url = '{}{}&magnet=1'.format(nyaa.dl_url, nyaa_id)
+    def __init__(self, nyaa, nyaa_id):
+        self.info_url = '{}{}'.format(nyaa.info_url, nyaa_id)
+        self.download_url = '{}{}&magnet=1'.format(nyaa.dl_url, nyaa_id)
 
-		r = _retry_on_fail(requests.get, self.info_url)
-		setattr(r, 'encoding', 'utf-8')
-		self.page = BeautifulSoup(r.text)
-		content = self.page.find('div', class_='content').text
-		if 'The torrent you are looking for does not appear to be in the database' in content:
-			self.exists = False
-		elif 'The torrent you are looking for has been deleted' in content:
-			self.exists = False
-		else:
-			self.exists = True
+        r = _retry_on_fail(requests.get, self.info_url)
+        setattr(r, 'encoding', 'utf-8')
+        self.page = BeautifulSoup(r.text)
+        content = self.page.find('div', class_='content').text
+        if 'The torrent you are looking for does not appear ' \
+           'to be in the database' in content:
+            self.exists = False
+        elif 'The torrent you are looking for has been deleted' in content:
+            self.exists = False
+        else:
+            self.exists = True
 
-	@property
-	def category(self):
-		return self.page.find('td', class_='viewcategory').find_all('a')[0].text
+    @property
+    def category(self):
+        return (
+            self.page.find('td', class_='viewcategory').find_all('a')[0].text
+        )
 
-	@property
-	def sub_category(self):
-		return self.page.find('td', class_='viewcategory').find_all('a')[1].text
+    @property
+    def sub_category(self):
+        return (
+            self.page.find('td', class_='viewcategory').find_all('a')[1].text
+        )
 
-	@property
-	def name(self):
-		return self.page.find('td', class_='viewtorrentname').text
+    @property
+    def name(self):
+        return self.page.find('td', class_='viewtorrentname').text
 
-	@property
-	def time(self):
-		return self.page.find('td', class_='vtop').text.split(', ')
+    @property
+    def time(self):
+        return self.page.find('td', class_='vtop').text.split(', ')
 
-	@property
-	def status(self):
-		_status = self.page.find('div', class_=re.compile('content'))['class']
-		if 'trusted' in _status:
-			return 'trusted'
-		elif 'remake' in _status:
-			return 'remake'
-		elif 'aplus' in _status:
-			return 'a+'
-		else:
-			return 'normal'
+    @property
+    def status(self):
+        _status = self.page.find('div', class_=re.compile('content'))['class']
+        if 'trusted' in _status:
+            return 'trusted'
+        elif 'remake' in _status:
+            return 'remake'
+        elif 'aplus' in _status:
+            return 'a+'
+        else:
+            return 'normal'
 
-	@property
-	def hash(self):
-		r = _retry_on_fail(requests.head, self.download_url)
-		if 'Location' in r.headers:
-			return re.search(r'magnet:\?xt=urn:btih:(.*)&tr=', r.headers['Location']).group(1).upper()
-		else:
-			return None
+    @property
+    def hash(self):
+        r = _retry_on_fail(requests.head, self.download_url)
+        if 'Location' in r.headers:
+            return re.search(r'magnet:\?xt=urn:btih:(.*)&tr=',
+                             r.headers['Location']).group(1).upper()
+        else:
+            return None
--- Scraper.py
+++ Scraper.py
@@ -5,82 +5,86 @@ import getopt
 import os
 import sys
 
+
 def load_settings():
-	class Configuration(object):
-		def __init__(self):
-			self.db_dir = os.path.dirname(os.path.realpath(__file__))
-			self.db_name = None
-			self.mode = None
-			self.nyaa_url = None
-			self.start_entry = None
+    class Configuration(object):
+        def __init__(self):
+            self.db_dir = os.path.dirname(os.path.realpath(__file__))
+            self.db_name = None
+            self.mode = None
+            self.nyaa_url = None
+            self.start_entry = None
 
-	config = Configuration()
+    config = Configuration()
 
-	arguments = sys.argv[1:]
-	optlist, args = getopt.getopt(arguments, '', ['start='])
-	if len(optlist) > 0:
-		for opt, arg in optlist:
-			if opt == '--start':
-				config.start_entry = arg
+    arguments = sys.argv[1:]
+    optlist, args = getopt.getopt(arguments, '', ['start='])
+    if len(optlist) > 0:
+        for opt, arg in optlist:
+            if opt == '--start':
+                config.start_entry = arg
 
-	if len(args) == 0:
-		print('Please supply arguments.')
-		exit(code=1)
-	elif len(args) == 1:
-		if args[0] == 'nyaa':
-			config.nyaa_url = 'http://www.nyaa.se/'
-			config.db_name = 'nyaa'
-		elif args[0] == 'sukebei':
-			config.nyaa_url = 'http://sukebei.nyaa.se/'
-			config.db_name = 'sukebei'
-		else:
-			print('Invalid url.')
-			exit(code=1)
-		config.mode = 'new'
-	else:
-		if args[0] == 'nyaa':
-			config.nyaa_url = 'http://www.nyaa.se/'
-			config.db_name = 'nyaa'
-		elif args[0] == 'sukebei':
-			config.nyaa_url = 'http://sukebei.nyaa.se/'
-			config.db_name = 'sukebei'
-		else:
-			print('Invalid url.')
-			exit(code=1)
-	
-		if args[1] == 'new':
-			config.mode = 'new'
-		elif args[1] == 'missed':
-			config.mode = 'missed'
-		else:
-			print('Invalid option.')
-			exit(code=1)
+    if len(args) == 0:
+        print('Please supply arguments.')
+        exit(code=1)
+    elif len(args) == 1:
+        if args[0] == 'nyaa':
+            config.nyaa_url = 'http://www.nyaa.se/'
+            config.db_name = 'nyaa'
+        elif args[0] == 'sukebei':
+            config.nyaa_url = 'http://sukebei.nyaa.se/'
+            config.db_name = 'sukebei'
+        else:
+            print('Invalid url.')
+            exit(code=1)
+        config.mode = 'new'
+    else:
+        if args[0] == 'nyaa':
+            config.nyaa_url = 'http://www.nyaa.se/'
+            config.db_name = 'nyaa'
+        elif args[0] == 'sukebei':
+            config.nyaa_url = 'http://sukebei.nyaa.se/'
+            config.db_name = 'sukebei'
+        else:
+            print('Invalid url.')
+            exit(code=1)
 
-	return config
+        if args[1] == 'new':
+            config.mode = 'new'
+        elif args[1] == 'missed':
+            config.mode = 'missed'
+        else:
+            print('Invalid option.')
+            exit(code=1)
+
+    return config
 
 config = load_settings()
 nt = Nyaa(config.nyaa_url)
 db = Database(config.db_dir, config.db_name)
 
-if config.mode == 'new' and config.start_entry == None:
-	config.start_entry = db.last_entry + 1
-elif config.mode == 'missed' and config.start_entry == None:
-	config.start_entry = 1
+if config.mode == 'new' and config.start_entry is None:
+    config.start_entry = db.last_entry + 1
+elif config.mode == 'missed' and config.start_entry is None:
+    config.start_entry = 1
 
 for i in range(config.start_entry, nt.last_entry + 1):
-	if config.mode == 'missed':
-		if db.entry_exists(i):
-			continue
+    if config.mode == 'missed':
+        if db.entry_exists(i):
+            continue
 
-	entry = NyaaEntry(nt, i)
-	if entry.exists == True:
-		if entry.category in db.categories and entry.sub_category in db.sub_categories:
-			print('Entry: {}, Name: {}'.format(i, entry.name))
-			info_hash = entry.hash
-			if info_hash == None:
-				print('Torrent aliased. Skipping.')
-				continue
-			db.write_torrent((i, entry.name, info_hash, db.categories[entry.category],
-				db.sub_categories[entry.sub_category], db.status[entry.status]))
+    entry = NyaaEntry(nt, i)
+    if entry.exists is True:
+        if entry.category in db.categories \
+                and entry.sub_category in db.sub_categories:
+            print('Entry: {}, Name: {}'.format(i, entry.name))
+            info_hash = entry.hash
+            if info_hash is None:
+                print('Torrent aliased. Skipping.')
+                continue
+            db.write_torrent((
+                i, entry.name, info_hash, db.categories[entry.category],
+                db.sub_categories[entry.sub_category], db.status[entry.status]
+            ))
 
 db.c.close()
